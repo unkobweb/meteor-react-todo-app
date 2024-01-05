12b35e12d4290d530c38b2b27f3b5744540874c6
unkobweb <sieg.alexandre@gmail.com>
2024-01-05 11:32:55 +0100

    feat(task): add update checkbox

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,6 +4,14 @@ import { Task } from './Task.jsx';
 import { TasksCollection } from '../api/TasksCollection.js';
 import { TaskForm } from './TaskForm.jsx';
 
+const toggleChecked = ({_id, isChecked}) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+}
+
 export const App = () => {
   const tasks = useTracker(() => TasksCollection.find({}, {sort: {createdAt: -1}}).fetch())
 
@@ -14,7 +22,7 @@ export const App = () => {
       <TaskForm />
 
       <ul>
-        {tasks.map(task => <Task key={task._id} task={task} />)}
+        {tasks.map(task => <Task key={task._id} task={task} onCheckboxClick={toggleChecked} />)}
       </ul>
     </div>
   )
