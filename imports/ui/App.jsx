946b93552be1b04a013b6c23eeb53335862a0bcf
unkobweb <sieg.alexandre@gmail.com>
2024-01-05 10:54:58 +0100

    feat(task): use mongo collection for tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
 import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
 import { Task } from './Task.jsx';
+import { TasksCollection } from '../api/TasksCollection.js';
 
-const tasks = [
-  {_id: 1, text: 'First Task'},
-  {_id: 2, text: 'Second Task'},
-  {_id: 3, text: 'Third Task'},
-];
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}).fetch())
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    <ul>
-      {tasks.map(task => <Task key={task._id} task={task} />)}
-    </ul>
-    <Hello/>
-    <Info/>
-  </div>
-);
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <ul>
+        {tasks.map(task => <Task key={task._id} task={task} />)}
+      </ul>
+      <Hello/>
+    </div>
+  )
+};
